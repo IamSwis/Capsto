f707b374614fd430a9536ee79b4e03db5a3a7427
IamSwis <dre12sims@gmail.com>
2024-07-17 14:19:56 -0400

    CApsto algo v2

---
+++ JS/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sendEmail = require('./sendEmail'); // Adjust the path according to the new structure
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.post('/send-email', (req, res) => {
+  console.log('Request body:', req.body); // Log the request body
+
+  const { name, email, phone, message } = req.body;
+  const to = 'jaweconsultingllc@gmail.com'; // my receiving email
+  const subject = `Message from ${name} (${email}, ${phone})`;
+  const text = message;
+
+  console.log(`Received request to send email to: ${to}, subject: ${subject}, text: ${text}`); // Log the individual fields
+
+  sendEmail(to, subject, text)
+    .then(info => {
+      console.log('Email sent:', info);
+      res.status(200).json({ success: true, info });
+    })
+    .catch(error => {
+      console.error('Error sending email:', error);
+      res.status(500).json({ success: false, error: error.message });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
